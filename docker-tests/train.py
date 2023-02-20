c4d431a3c14efe6f566a088dd5fc62652739f478
Kristian <knudsenkristian123@gmail.com>
2023-02-20 11:50:47 +0100

    testing docker

---
+++ docker-tests/train.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+import torch
+import matplotlib
+import tqdm
+import segmentation_models_pytorch
+
+def main():
+    print("Hellor docker")
+
+if __name__ == "__main__":
+    main()
