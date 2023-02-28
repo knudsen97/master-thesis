82a5ce0a08b46caf86999768ab7da6fe2abcde7d
Claus Huynh <claus.huynh@yahoo.dk>
2023-02-28 09:40:53 +0100

    updated bash

--- blender/generate_image.sh
+++ blender/generate_image.sh
@@ -1,40 +1,34 @@
 #!/bin/bash
+Help()
+{
+   # Display Help
+   echo "Generates synthetic images. Calling the script without any arguments will generate 2 images."
+   echo
+   echo "Syntax: scriptTemplate [-n|h|r]"
+   echo "options:"
+   echo "n     The number of images to generate. It will generate 2*n images."
+   echo "h     Print this Help."
+   echo "r     Removes images from folder depth, images, and label."
+}
+images=1
+# Read the options
+while getopts n:hr flag
+do
+    case "${flag}" in
+        n) images=${OPTARG:-1};;
+        h) Help
+           exit;;
+        r) rm ./depth/*.png
+           rm ./image/*.png
+           rm ./label/*.png
+           exit;;
+    esac
+done
 
-# Make for loop based on arguments
-# for i in "$@"
-# do
-#     blender -b synthetic_data_generator.blend --python /home/claus/Documents/sdu/9sem/master/code/master-thesis/blender/blender_script/blender_render_script.py -f 40,41
-
-#     # Remove nonsens image
-#     rm ./depth/*41*.png
-#     rm ./label/*41*.png
-#     rm ./image/*40*.png
-
-#     # Rename image
-#     mv ./depth/*_L.png ./depth/depth$((i*2)).png
-#     mv ./depth/*_R.png ./depth/depth$((i*2+1)).png
-
-#     mv ./label/*_L.png ./label/label$((i*2)).png
-#     mv ./label/*_R.png ./label/label$((i*2+1)).png
-
-#     mv ./image/*_L.png ./image/image$((i*2)).png
-#     mv ./image/*_R.png ./image/image$((i*2+1)).png
-
-#     # mv ./depth/*_L.png ./depth/depth1.png
-#     # mv ./depth/*_R.png ./depth/depth2.png
-
-#     # mv ./label/*_L.png ./label/label1.png
-#     # mv ./label/*_R.png ./label/label2.png
-
-#     # mv ./image/*_L.png ./image/image1.png
-#     # mv ./image/*_R.png ./image/image2.png
-# done
-
-# print i from 1 to argument given
-for i in $(seq 0 $(($1-1)))
+for i in $(seq 0 $(($images-1)))
 do
     blender -b synthetic_data_generator.blend --python /home/claus/Documents/sdu/9sem/master/code/master-thesis/blender/blender_script/blender_render_script.py -f 40,41
-    echo "Image $i generated"
+    echo "$((($i+1)*2)) images generated"
     # Remove nonsens image
     rm ./depth/*41*.png
     rm ./label/*41*.png
@@ -50,12 +44,4 @@ do
     mv ./image/*_L.png ./image/image$((i*2)).png
     mv ./image/*_R.png ./image/image$((i*2+1)).png
 
-    # mv ./depth/*_L.png ./depth/depth1.png
-    # mv ./depth/*_R.png ./depth/depth2.png
-
-    # mv ./label/*_L.png ./label/label1.png
-    # mv ./label/*_R.png ./label/label2.png
-
-    # mv ./image/*_L.png ./image/image1.png
-    # mv ./image/*_R.png ./image/image2.png
 done
