d12d7e23f1892bdf8c61b0285e1256bdd706f07b
Claus Huynh <claus.huynh@yahoo.dk>
2023-03-06 10:19:21 +0100

    forgot to commit the script and updated blend file

--- blender/blender_script/blender_render_script.py
+++ blender/blender_script/blender_render_script.py
@@ -4,102 +4,134 @@ import math
 import numpy as np
 import sys
 from mathutils import Vector
+from copy import copy
+import importlib
+import random
 
-def delete_all_boxes():
-    """detete all boxes except the original box
+try:
+    import gt_face_change_script
+    importlib.reload(gt_face_change_script)
+except ImportError:
+    # Include the path to the blender_script folder
+    path_to_file = os.path.dirname(__file__)
+    
+    # Delete the last part of the path
+    path_to_file = path_to_file.split("/")
+    path_to_file = path_to_file[:-1]
+    path_to_file = "/".join(path_to_file)
+    
+    # Add blender_script to the path
+    path_to_file = path_to_file + "/blender_script"
+    
+    # Add the path to the sys.path and import
+    sys.path.append(path_to_file)
+    import gt_face_change_script
+
+def delete_all_boxes(collection = "copies"):
+    """detete all boxes in a collection
+
+    Args:
+        collection (str, optional): Name of the collection. Defaults to "copies".
     """
     # Get all objects in the scene
-    objects = bpy.data.objects
+    objects = bpy.data.collections[collection].all_objects
     # Delete all objects that have the name "box" excluding the original box
     for obj in objects:
-        if obj.name.startswith("box") and obj.name != "box":
-            bpy.data.objects.remove(obj)
+        bpy.data.objects.remove(obj)
 
-def add_box_at_random(number_of_boxes):
+def add_box_at_random(number_of_boxes, _boxes):
     """Generate a random amount of boxes arround the container
 
     Args:
-        number_of_boxes (int): Number of boxes to add, max 5*5*4
+        number_of_boxes (int): Number of boxes to add
+        _boxes (list): List of all boxes that can be added
     """
     original_location = Vector((0.09, 0.5, 0.16))
     box_size = (0.05, 0.09, 0.02) # (x, y, z)
     offset = 0.01
+    x_min = original_location[0]
+    x_max = original_location[0] + box_size[0]*5+offset
+    y_min = original_location[1] - box_size[1]*4-offset
+    y_max = original_location[1]
+    z_min = original_location[2]
+    z_max = original_location[2] + box_size[2]*7+offset
 
-    # Get instance of box
-    original_box = bpy.data.objects["box"]
+    # Get list of item in collection boxes
+    boxes = copy(_boxes)
+    random.shuffle(boxes)
 
-    # make array of possible box locations
-    x = original_location[0]
-    y = original_location[1]
-    z = original_location[2]
-    x = np.arange(
-        original_location[0], 
-        original_location[0] + box_size[0]*5+offset, 
-        box_size[0]+offset
-        ) 
-    y = np.arange( # Here we go in the negative direction
-        original_location[1] - box_size[1]*4-offset, # y direction is a bit tight
-        original_location[1], 
-        box_size[1]+offset
-        ) 
-    z = np.arange(
-        original_location[2], 
-        original_location[2] + box_size[2]*6+offset, 
-        box_size[2]+offset
-        )
 
-    # Make a list of all possible locations
-    locations = []
-    for i in x:
-        for j in y:
-            for k in z:
-                locations.append((i, j, k))
-    
-    # Shuffle the locations
-    np.random.shuffle(locations)
-
+    added_boxes = []
     # Add boxes to the scene
+        # bpy.context.scene.rigidbody_world.enabled = True
+
+
     for i in range(0, number_of_boxes):
+        # Shuffle list of boxes
+
+        if len(boxes) == 0:
+            boxes = copy(_boxes)
+            random.shuffle(boxes)
+        original_box = boxes.pop()
+
+
+        # Generate a random location
+        x = np.random.uniform(x_min, x_max)
+        y = np.random.uniform(y_min, y_max)
+        z = np.random.uniform(z_min, z_max)
+        locations = Vector((x, y, z))
+
         # Duplicate the original box
         new_box = original_box.copy()
         new_box.data = original_box.data.copy()
         new_box.animation_data_clear()
 
         # Set the location of the new box
-        new_box.location = locations[i]
+        new_box.location = locations
+        new_box.rigid_body.enabled = True
 
-        # Add the new box to the scene
-        bpy.context.scene.collection.objects.link(new_box)
+        # Add the new box to the scene in collection 'copies'
+        bpy.data.collections['copies'].objects.link(new_box)
+        added_boxes.append(new_box)
+
+    return added_boxes
 
 
-
-
+def face_change_handler(FaceColor: gt_face_change_script.FaceColor):
+    """Creates the handler that changes the face of the boxes
+    """
+    def handler(scene):
+        """Handler that changes the face of the boxes"""
+        frame_num = scene.frame_current
+        if frame_num > 40 and not FaceColor.changed:
+            FaceColor.paint_gt()    
+        elif FaceColor.changed:
+            FaceColor.return_face_texture()
+    return handler
 
 def main():
-    # Try to import the gt_face_change_script
-    try:
-        import gt_face_change_script
-    except ImportError:
-        # Include the path to the blender_script folder
-        path_to_file = os.path.dirname(__file__)
-        
-        # Delete the last part of the path
-        path_to_file = path_to_file.split("/")
-        path_to_file = path_to_file[:-1]
-        path_to_file = "/".join(path_to_file)
-        
-        # Add blender_script to the path
-        path_to_file = path_to_file + "/blender_script"
-        
-        # Add the path to the sys.path and import
-        sys.path.append(path_to_file)
-        import gt_face_change_script
 
     # Reset scene
     # bpy.context.scene.rigidbody_world.enabled = False
-    rng = np.random.randint(0, 5*5*4, 1).item() # Random number between 0 and 5*5*4
-    delete_all_boxes()
-    add_box_at_random(rng)
+    rng = np.random.randint(0, 50, 1).item() # Random number between 0 and 5*5*4
+    try:
+        delete_all_boxes()
+    except NameError:
+        pass
+
+    # box = bpy.data.objects['box']
+    # boxes = [box]
+    boxes = [obj for obj in bpy.data.collections['boxes'].all_objects]
+    
+    added_boxes = add_box_at_random(rng, boxes)
+
+    face_changer = gt_face_change_script.FaceColor(added_boxes)
+
+    # append face change handler
+    def register(handler_function):
+        bpy.app.handlers.frame_change_post.append(handler_function)
+
+    register(face_change_handler(face_changer))
 
     try:
         # Free baking
@@ -122,27 +154,8 @@ def main():
 
     # Jump to frame 40
     bpy.context.scene.frame_set(40)
-    # Wait for frame to set
-    gt_face_change_script.main()
-    bpy.context.scene.frame_set(1)
+    bpy.context.scene.frame_set(41)
 
-    # call update in 1 sec
-    # bpy.app.timers.register(update, first_interval=1)
-    # bpy.context.scene.rigidbody_world.enabled = False
-    # # Render the image
-    # bpy.ops.render.render(write_still=True)
-
-    # # jump to frame 41
-    # bpy.context.scene.frame_set(41)
-
-    # # Render the image
-    # bpy.ops.render.render(write_still=True)
-
-
-
-
-
-    # gt_face_change_script.main()
 
 if __name__ == "__main__":
     main()
--- blender/blender_script/gt_face_change_script.py
+++ blender/blender_script/gt_face_change_script.py
@@ -2,7 +2,8 @@ import bpy
 import numpy as np
 import math
 from mathutils import Vector
-
+import threading
+import copy
 # TODO: Make raycast direction = normal of top face
 
 face_coord_gt_red = np.array([[0.00, 0.00], [0.0, 1.00], [0.50, 1.00], [0.50, 0.00]])
@@ -10,158 +11,180 @@ face_coord_gt_green = np.array([[0.5, 0.00], [0.5, 1.00], [1.00, 1.00], [1.00, 0
 
 OBJECT_ANGLE_SUCTION_THRESHOLD = 2*math.pi/8
 
+class FaceColor:
+    def __init__(self, box_objects_array):
+        # Get all objects that start with "box"
+        self.box_objects = box_objects_array
 
-def update_face_color(faces, uv_layer, color):
-    """Update the color of the faces
-    
-    Args:
-        faces (bpy.object.data.polygon): Only expecting 8 faces
-        uv_layer (obj.data.uv_layers.active.data): Uv layer of the object
-        color (np.array): Color to set the faces to
+        self.box_original_uv_layer_values = []
+        for obj in self.box_objects:
+            uv_layer = obj.data.uv_layers.active.data
+            uv_layer_values = []
+            for face in obj.data.polygons:
+                uv_values = [copy.copy(uv_layer[face.loop_start + vert_idx].uv) for vert_idx in range(4)]
+                uv_layer_values.append(uv_values)
+            self.box_original_uv_layer_values.append(uv_layer_values)
+        # self.box_original_uv_layer = [obj.data.uv_layers.active.data for obj in self.box_objects]
+
+        # Get scene
+        self.scene = bpy.context.scene
+
+        # Get depsgraph
+        self.depsgraph = bpy.context.evaluated_depsgraph_get()
+        self.changed = False
+    # def update_face_color(self, faces, uv_layer, color):
+    #     """Update the color of the faces
         
-    Returns:
-        None
-    """
-    vert_indices = 4 # Only expect to be 4 vertices per face
-    for face in faces:
-        for i, vert_idx in enumerate(face.vertices):
-        # for vert_idx in range(vert_indices):
-            uv_layer[vert_idx].uv = color[i] # Set all uv coordinates to green
+    #     Args:
+    #         faces (bpy.object.data.polygon): Only expecting 8 faces
+    #         uv_layer (obj.data.uv_layers.active.data): Uv layer of the object
+    #         color (np.array): Color to set the faces to
+            
+    #     Returns:
+    #         None
+    #     """
+    #     vert_indices = 4 # Only expect to be 4 vertices per face
+    #     for face in faces:
+    #         for i, vert_idx in enumerate(face.vertices):
+    #         # for vert_idx in range(vert_indices):
+    #             uv_layer[vert_idx].uv = color[i] # Set all uv coordinates to green
 
-def classify_faces(faces, rotation = None):
-    """Classify faces as green or red (graspable or not graspable)
+    def classify_faces(self, faces, rotation = None):
+        """Classify faces as green or red (graspable or not graspable)
 
-    Args:
-        faces (bpy.object.data.polygon): Only expecting 8 faces
+        Args:
+            faces (bpy.object.data.polygon): Only expecting 8 faces
 
-    Returns:
-        touple: Two list of faces, green and red
-    """
-    green_faces = []
-    red_faces = []
-    # Find the angle of each faces
-    print(f"from classify_faces: len(faces): {len(faces)}")
-    for face in faces:
-        normal = face.normal
-        # Get the normal of the face
-        if rotation is not None:
-            normal = rotation @ normal
-        print(f"normal: {normal}")
+        Returns:
+            touple: Two list of faces, green and red
+        """
+        green_faces = []
+        red_faces = []
+        # Find the angle of each faces
+        # print(f"from classify_faces: len(faces): {len(faces)}")
+        for face in faces:
+            normal = face.normal
+            # Get the normal of the face
+            if rotation is not None:
+                normal = rotation @ normal
+            # print(f"normal: {normal}")
 
-        # angle = math.acos(normal.dot((0, 0, 1))) # Calculate the angle relative to the XY plane
-        angle = normal.angle(Vector((0, 0, 1)))
-        print(f"angle: {angle}")
-        # if obj.name == "box.086":
-        #     print(f"Box: {obj.name} face: {face.index} angle: {angle} radians")
-        if -OBJECT_ANGLE_SUCTION_THRESHOLD < angle < OBJECT_ANGLE_SUCTION_THRESHOLD:
-            green_faces.append(face)
-        else:
-            print(f"not in threshold was: {angle}")
-            red_faces.append(face)
-    return green_faces, red_faces
-
-
-def update_face_color(faces, uv_layer, color):
-    """Update the color of the faces
-    
-    Args:
-        faces (bpy.object.data.polygon): Only expecting 8 faces
-        uv_layer (obj.data.uv_layers.active.data): Uv layer of the object
-        color (np.array): Color to set the faces to
-        
-    Returns:
-        None
-    """
-    color_str = "green" if color[0][0] == 0.5 else "red"
-    print(f"len(faces): {len(faces)} color: {color_str}")
-    vert_indices = 4 # Only expect to be 4 vertices per face
-    for face in faces:
-        for vert_idx in range(vert_indices):
-            uv_layer[face.loop_start + vert_idx].uv = color[vert_idx] # Set all uv coordinates to green
-
-def get_top_faces(mesh, transformation = None):
-    """Get the top faces of the mesh
-    TODO: does not seems to work?
-    Args:
-        mesh (bpy.object.data): Mesh of the object
-        
-    Returns:
-        list: List of top faces
-    """
-    # Get top faces
-    max_z = 0
-    top_faces = []
-    vertecise = mesh.vertices
-
-    
-    z_coords = None
-    if transformation is not None:
-        z_coords = [(v.co @ transformation).z for v in vertecise]
-    else:
-        z_coords = [v.co.z for v in vertecise]
-    
-    max_z = max(z_coords)
-    print(f"from get_top_faces: len(mesh.polygons): {len(mesh.polygons)}")
-    for face in mesh.polygons:
-        for vert_idx in face.vertices:
-            co = None
-            if transformation is not None:
-                co = (vertecise[vert_idx].co @ transformation)
+            # angle = math.acos(normal.dot((0, 0, 1))) # Calculate the angle relative to the XY plane
+            angle = normal.angle(Vector((0, 0, 1)))
+            # print(f"angle: {angle}")
+            # if obj.name == "box.086":
+            #     print(f"Box: {obj.name} face: {face.index} angle: {angle} radians")
+            if -OBJECT_ANGLE_SUCTION_THRESHOLD < angle < OBJECT_ANGLE_SUCTION_THRESHOLD:
+                green_faces.append(face)
             else:
-                co = (vertecise[vert_idx].co)
-
-            if co.z == max_z:
-                top_faces.append(face)
-                print(f"face: {face.index} max_z: {max_z}")
-                break
-
-    return top_faces
-
-def main():
-    # Get all objects that start with "box"
-    box_objects = [obj for obj in bpy.data.objects if obj.name.startswith("box")]
-
-    # Get scene
-    scene = bpy.context.scene
-
-    # Get depsgraph
-    depsgraph = bpy.context.evaluated_depsgraph_get()
+                # print(f"not in threshold was: {angle}")
+                red_faces.append(face)
+        return green_faces, red_faces
 
 
-    for box in box_objects:
-        mesh = box.data
-        uv_layer = box.data.uv_layers.active.data
+    def update_face_color(self, faces, uv_layer, color):
+        """Update the color of the faces
+        
+        Args:
+            faces (bpy.object.data.polygon): Only expecting 8 faces
+            uv_layer (obj.data.uv_layers.active.data): Uv layer of the object
+            color (np.array): Color to set the faces to
+            
+        Returns:
+            None
+        """
+        # color_str = "green" if color[0][0] == 0.5 else "red"
+        # print(f"len(faces): {len(faces)} color: {color_str}")
+        vert_indices = 4 # Only expect to be 4 vertices per face
+        for face in faces:
+            for vert_idx in range(vert_indices):
+                uv_layer[face.loop_start + vert_idx].uv = color[vert_idx] # Set all uv coordinates to green
 
-        # Get the evaluated object from the depsgraph
-        evaluated_object = box.evaluated_get(depsgraph)
+    def get_top_faces(self, mesh, transformation = None):
+        """Get the top faces of the mesh
+        TODO: does not seems to work?
+        Args:
+            mesh (bpy.object.data): Mesh of the object
+            
+        Returns:
+            list: List of top faces
+        """
+        # Get top faces
+        max_z = 0
+        top_faces = []
+        vertecise = mesh.vertices
 
-        # Get the location of the evaluated object
-        loc = evaluated_object.matrix_world.translation
-        rot = evaluated_object.matrix_world.to_3x3()
-        transform = evaluated_object.matrix_world
+        
+        z_coords = None
+        if transformation is not None:
+            z_coords = [(v.co @ transformation).z for v in vertecise]
+        else:
+            z_coords = [v.co.z for v in vertecise]
+        
+        max_z = max(z_coords)
+        # print(f"from get_top_faces: len(mesh.polygons): {len(mesh.polygons)}")
+        for face in mesh.polygons:
+            for vert_idx in face.vertices:
+                co = None
+                if transformation is not None:
+                    co = (vertecise[vert_idx].co @ transformation)
+                else:
+                    co = (vertecise[vert_idx].co)
 
-        hit, pos, normal, face_id, obj, matrix = scene.ray_cast(depsgraph, loc, (0,0,1)) 
+                if co.z == max_z:
+                    top_faces.append(face)
+                    # print(f"face: {face.index} max_z: {max_z}")
+                    break
 
-        i = 0
-        offset = Vector((0, 0, 0.001))
-        while hit and obj.name == box.name:
-            hit, pos, normal, face_id, obj, matrix = scene.ray_cast(depsgraph, pos, (0,0,1))
-            i += 1
-            pos = pos + offset
-            if i > 100:
-                print(f"box: {box.name}: hit 100 times")
-                break
+        return top_faces
 
-        # start with all face red
-        update_face_color(mesh.polygons, uv_layer, face_coord_gt_red)
+    def paint_gt(self):
+        """Paint the ground truth on the objects
+        """
+        self.changed = True
+        for box in self.box_objects:
+            mesh = box.data
+            uv_layer = box.data.uv_layers.active.data
 
-        if not hit:
-            # Find top faces based on top vertex
-            # top_faces = get_top_faces(mesh, transform)
-            green_faces, red_faces = classify_faces(mesh.polygons, rot)
+            # Get the evaluated object from the depsgraph
+            evaluated_object = box.evaluated_get(self.depsgraph)
 
-            update_face_color(green_faces, uv_layer, face_coord_gt_green)
-            update_face_color(red_faces, uv_layer, face_coord_gt_red)    
+            # Get the location of the evaluated object
+            loc = evaluated_object.matrix_world.translation
+            rot = evaluated_object.matrix_world.to_3x3()
+            transform = evaluated_object.matrix_world
 
-if __name__ == "__main__":
-    main()
+            hit, pos, normal, face_id, obj, matrix = self.scene.ray_cast(self.depsgraph, loc, (0,0,1)) 
+
+            i = 0
+            offset = Vector((0, 0, 0.001))
+            while hit and obj.name == box.name:
+                hit, pos, normal, face_id, obj, matrix = self.scene.ray_cast(self.depsgraph, pos, (0,0,1))
+                i += 1
+                pos = pos + offset
+                if i > 100:
+                    print(f"box: {box.name}: hit 100 times")
+                    break
+
+            # start with all face red
+            self.update_face_color(mesh.polygons, uv_layer, face_coord_gt_red)
+
+            if not hit:
+                # Find top faces based on top vertex
+                # top_faces = get_top_faces(mesh, transform)
+                green_faces, red_faces = self.classify_faces(mesh.polygons, rot)
+
+                self.update_face_color(green_faces, uv_layer, face_coord_gt_green)
+                self.update_face_color(red_faces, uv_layer, face_coord_gt_red)    
+
+    def return_face_texture(self):
+        """Return the texture of the objects to its original state
+        """
+        self.changed = False
+        vert_indices = 4 # Only expect to be 4 vertices per face
+        for obj, uv_data_values in zip(self.box_objects, self.box_original_uv_layer_values):
+            uv_layer = obj.data.uv_layers.active.data
+            for face, uv_data in zip(obj.data.polygons, uv_data_values):
+                for vert_idx in range(vert_indices):
+                    uv_layer[face.loop_start + vert_idx].uv = uv_data[vert_idx]
+        
