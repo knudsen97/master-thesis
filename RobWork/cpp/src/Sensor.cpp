66aea7ab91f889ae99eb57d573f3a7852e6b8ba1
Kristian Knudsen <knudsenkristian123@gmail.com>
2023-04-13 12:12:31 +0200

    Added noise function, cleanup and documentation

--- RobWork/cpp/src/Sensor.cpp
+++ RobWork/cpp/src/Sensor.cpp
@@ -1,9 +1,16 @@
 #include "../inc/Sensor.hpp"
 
-// Default constructor
+/**
+ * @brief Default constructor for SimulatedRGBD object.
+*/
 SimulatedRGBD::SimulatedRGBD(){}
 
-// Constructor
+/**
+ * @brief Constructor for SimulatedRGBD object.
+ * @param sim_cam The simulated camera to use.
+ * @param sim_depth The simulated depth scanner to use.
+ * @param intrinsics The intrinsics of the camera. If no intrinsics are provided, the default intrinsics are used.
+*/
 SimulatedRGBD::SimulatedRGBD(const SimulatedCamera &sim_cam, const SimulatedScanner25D &sim_depth, const cv::Mat &intrinsics) 
     : sim_cam( ownedPtr (new SimulatedCamera(sim_cam)) ),
         sim_depth( ownedPtr (new SimulatedScanner25D(sim_depth)) )
@@ -21,10 +28,16 @@ SimulatedRGBD::SimulatedRGBD(const SimulatedCamera &sim_cam, const SimulatedScan
     {
         this->intrinsics = intrinsics;
     }
-
     // std::cout << "Intrinsics: \n" << this->intrinsics << std::endl;
 }
 
+
+/**
+ * @brief Convert RobWork Image to OpenCV Mat
+ * @param image Raw pointer to RobWork Image
+ * @param cv_image OpenCV Mat to store the converted image
+ * @param imgType Type of image to convert to. Use ImageType::RGB or ImageType::BGR
+*/
 void SimulatedRGBD::convertImageToCV(const Image *image, cv::Mat &cv_image, int imgType)
 {
     cv::Mat _image = cv::Mat(image->getHeight(), image->getWidth(), CV_8UC3, (Image*)image->getImageData());
@@ -43,6 +56,10 @@ void SimulatedRGBD::convertImageToCV(const Image *image, cv::Mat &cv_image, int 
 }
 
 
+/**
+ * @brief Initialize the simulated RGB camera
+ * @param fps Frame rate of the simulated camera
+*/
 void SimulatedRGBD::initCamera(int fps)
 {
     this->sim_cam->setFrameRate(fps);
@@ -50,12 +67,23 @@ void SimulatedRGBD::initCamera(int fps)
     this->sim_cam->start();
 }
 
+
+/**
+ * @brief Initialize the simulated depth scanner
+ * @param fps Frame rate of the simulated scanner
+*/
 void SimulatedRGBD::initScanner25D(int fps)
 {
     this->sim_depth->setFrameRate(fps);
     this->sim_depth->open();
 }
 
+
+/**
+ * @brief Acquire image from simulated RGBD RealSense sensor. This does not get the image, but waits for the image to be ready.
+ * @param state RobWork state of the workcell
+ * @param info Simulator update info
+*/
 void SimulatedRGBD::acquireImage(State &state, const Simulator::UpdateInfo &info)
 {
     std::cout << "Acquiring image from simulated RGBD Sensor..." << std::endl;
@@ -72,6 +100,12 @@ void SimulatedRGBD::acquireImage(State &state, const Simulator::UpdateInfo &info
 
 }
 
+
+/**
+ * @brief Acquire depth image from simulated RGBD RealSense sensor. This does not get the depth image, but waits for the depth image to be ready.
+ * @param state RobWork state of the workcell
+ * @param info Simulator update info
+*/
 void SimulatedRGBD::acquireDepth(State &state, const Simulator::UpdateInfo &info)
 {
     std::cout << "Acquiring depth image from simulated RGBD Sensor..." << std::endl;
@@ -87,13 +121,23 @@ void SimulatedRGBD::acquireDepth(State &state, const Simulator::UpdateInfo &info
     std::cout << "Depth image ready!" << std::endl;
 }
 
-
+/**
+ * @brief Get image from simulated RGBD RealSense sensor.
+ * @param image_out Image in OpenCV format
+ * @param imgType Image type. Use ImageType::RGB or ImageType::BGR
+*/
 void SimulatedRGBD::getImage(cv::Mat &image_out, int imgType)
 {
     const Image *image = this->sim_cam->getImage();
     SimulatedRGBD::convertImageToCV(image, image_out, imgType);
 }
 
+
+/**
+ * @brief Get point cloud and depth image from simulated RGBD RealSense sensor.
+ * @param pc_out Point cloud pointer in Open3D format
+ * @param depthImage_out Depth image in OpenCV format
+*/
 void SimulatedRGBD::getPointCloudAndDepthImage(PointCloudPtr &pc_out, cv::Mat &depthImage_out)//, const cv::Mat &R, const cv::Mat &t);
 {
     const rw::geometry::PointCloud::Ptr pc = ownedPtr( new rw::geometry::PointCloud(this->sim_depth->getScan()) );
@@ -140,26 +184,57 @@ void SimulatedRGBD::getPointCloudAndDepthImage(PointCloudPtr &pc_out, cv::Mat &d
         
     }
     cv::flip(depthImage, depthImage_out, 1);
-
-    // Cout depth values around cv::Point(320, 240). Radius 4:
-    // for (int x = 316; x < 324; x++)
-    // {
-    //     for (int y = 236; y < 244; y++)
-    //     {
-    //         std::cout << depthImage.at<int16_t>(y, x) << ", ";
-    //     }
-    //     std::cout << std::endl;
-    // }
 }
 
 
+/**
+ * @brief Adds gaussian noise to the depth image.
+ * @param depth The input depth image that gets noise added to it.
+ * @param mean The mean used for noise generation.
+ * @param stddev The standard deviation used for noise generation.
+ * @param scale The scale used for noise generation.
+*/
+void SimulatedRGBD::addDepthNoise(cv::Mat &depth, double mean, double stddev, double scale)
+{
+    cv::Mat noise = cv::Mat::zeros(depth.size(), CV_64F);
+    cv::randn(noise, mean, stddev);
+    noise = noise * scale;
 
+    for (int i = 0; i < noise.rows; i++)
+    {
+        for (int j = 0; j < noise.cols; j++)
+        {
+            // Scale noise and cast to uint16_t
+            auto d = depth.at<uint16_t>(i, j);
+
+            // If depth is 0, point is invalid
+            if (d == 0)
+                continue;
+
+            // Add noise to depth
+            int val = d + round(noise.at<double>(i, j));
+            if (val < 0)
+                val = 0;
+
+            depth.at<uint16_t>(i, j) = val;
+        }
+    }
+}
+
+
+/**
+ * @brief Closes RobWork simulated camera and depth sensor.
+*/
 void SimulatedRGBD::close()
 {
     this->sim_cam->stop();
     this->sim_depth->close();
 }
 
+
+/**
+ * @brief Destructor.
+*/
 SimulatedRGBD::~SimulatedRGBD()
 {
     // this->sim_cam->stop();
