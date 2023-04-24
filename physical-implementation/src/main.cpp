c73920361abc32d4c51535ea0ea006de4143e08a
Kristian Knudsen <knudsenkristian123@gmail.com>
2023-04-24 15:45:24 +0200

    Implemented new calibration methods in main and cleanup

--- physical-implementation/src/main.cpp
+++ physical-implementation/src/main.cpp
@@ -11,30 +11,14 @@
 #include "../inc/Sensor.hpp"
 #include "../inc/PredictionProcessor.hpp"
 #include "../inc/Inference.hpp"
+#include "../inc/CameraCalibration.hpp"
 
 
 typedef std::shared_ptr<open3d::geometry::PointCloud> PointCloudPtr;
 
 
-void open3d_to_cv(open3d::t::geometry::Image& open3d_image, cv::Mat& cv_image, bool is_color = true)
-{
-    if (is_color)
-    {
-        cv::Mat cv_image_copy(open3d_image.GetRows(), open3d_image.GetCols(),
-                    CV_8UC(open3d_image.GetChannels()),
-                    open3d_image.GetDataPtr());
-        cv::cvtColor(cv_image_copy, cv_image_copy, cv::COLOR_RGB2BGR);
-        cv_image_copy.copyTo(cv_image);
-    }
-    else
-    {
-        cv::Mat cv_image_copy(open3d_image.GetRows(), open3d_image.GetCols(),
-                    CV_16UC(open3d_image.GetChannels()),
-                    open3d_image.GetDataPtr());
-        cv_image_copy.copyTo(cv_image);
-    }
-}
-
+void load_intrinsics(const std::string &filename, cv::Mat &intrinsics_out, cv::Mat &dist_coeff_out);
+void load_extrinsics(const std::string &filename, cv::Mat &extrinsics);
 
 int main(int argc, char* argv[])
 {
@@ -45,8 +29,11 @@ int main(int argc, char* argv[])
         return 0;
     }
 
+
+    //-------------------------
     std::string model_file_name = "unet_resnet101_1_jit.pt";
     std::string model_name;
+    bool find_extrinsics = false;
 
     for (size_t i = 0; i < argc; i++)
     {
@@ -58,24 +45,72 @@ int main(int argc, char* argv[])
         {
             model_name = argv[i + 1];
         }
+        else if (std::string(argv[i]) == "--find_extrinsics")
+        {
+            std::string arg = argv[i + 1];
+            // Change string to lowercase
+            std::transform(arg.begin(), arg.end(), arg.begin(), [](unsigned char c){ return std::tolower(c); });
+            std::cout << "arg: " << arg << std::endl;
+            if (arg == "true")
+                find_extrinsics = true;
+        }
     }
     if (model_name.empty())
     {
         model_name = model_file_name.substr(0, model_file_name.size() - 7);
     }
 
+    // Create PredictionProcessor object
+    double depth_scale = 1e4;
+    PredictionProcessor processor(depth_scale);
+
+    // Create Inference object
+    cv::Mat image, pred_image, depth;
+    cv::Mat returned_image;
+    bool inference_sucess;
+    Inference inf("../../models/" + model_file_name);
+
     // Load camera configuration into a Sensor object
     const std::string config_filename = "../config/camera_config.json";
     Sensor sensor(config_filename);
+    
+    // Start capture
+    sensor.startCapture();
 
     // Create intrinsics and extrinsics matrices
-    Eigen::Matrix4d extrinsics = Eigen::Matrix4d::Identity();
-    cv::Mat intrinsics;
+    Eigen::Matrix4d extrinsics_eigen = Eigen::Matrix4d::Identity();
     open3d::camera::PinholeCameraIntrinsic camera_intrinsics;
+    cv::Mat intrinsics, extrinsics, dist_coeff;
 
-    // Set intrinsics and extrinsics
-    sensor.setIntrinsics(50.0);
-    sensor.setExtrinsics(extrinsics);
+    // CAMERA CALIBRATION (EXTRINSICS)
+    cv::Size board_size(13, 9);
+    cv::Size image_size(640, 480);
+    int square_size = 19;
+    CameraCalibration calib(board_size, image_size, square_size);
+
+    // Load intrinsics and extrinsics from YAML files
+    cv::FileStorage fs;
+    load_intrinsics("../config/calibration.yaml", intrinsics, dist_coeff);
+    if(!intrinsics.empty())
+        sensor.setIntrinsics(intrinsics);
+    else // use default intrinsics
+        sensor.setIntrinsics(50);
+
+    // Estimate extrinsics of the camera or load from YAML file
+    if (find_extrinsics)
+        extrinsics = calib.calculateExtrinsics(sensor, "../config/calibration.yaml");
+    else // Load extrinsics from YAML file
+        load_extrinsics("../config/extrinsics.yaml", extrinsics);
+
+    // sensor.setExtrinsics(extrinsics); // This does not work if the pointcloud
+    sensor.setExtrinsics(extrinsics_eigen); // Has to be identity matrix for pointcloud
+
+    // Close all cv windows
+    cv::destroyAllWindows();
+
+    // Print intrinsic and extrinsics found/loaded
+    std::cout << "Intrinsics:\n" << intrinsics << std::endl;
+    std::cout << "Extrinsics:\n" << extrinsics << std::endl;
 
     // Create flip matrix to flip the point cloud
     Eigen::Matrix4d flip_mat;
@@ -83,18 +118,6 @@ int main(int argc, char* argv[])
                 0, -1, 0, 0,
                 0, 0, -1, 0,
                 0, 0, 0, 1;
-    
-    // Create PredictionProcessor object
-    double depth_scale = 1e4;
-    PredictionProcessor processor(depth_scale);
-
-    cv::Mat image, pred_image, depth;
-    cv::Mat returned_image;
-    bool inference_sucess;
-    Inference inf("../../models/" + model_file_name);
-
-    // Start capture
-    sensor.startCapture();
 
     while(true)
     {
@@ -105,16 +128,16 @@ int main(int argc, char* argv[])
         sensor.grabFrame(open3d_image, open3d_depth);
 
         // Convert Open3D image to OpenCV image
-        open3d_to_cv(open3d_image, image, true);
-        open3d_to_cv(open3d_depth, depth, false);
+        sensor.open3d_to_cv(open3d_image, image, true);
+        sensor.open3d_to_cv(open3d_depth, depth, false);
 
         // Create point cloud
         auto open3d_depth_legacy = open3d_depth.ToLegacy();
         sensor.createPinholeCameraIntrinsics(camera_intrinsics);
-        sensor.getExtrinsics(extrinsics);
+        sensor.getExtrinsics(extrinsics_eigen);
         int depth_scale = 1000;
         PointCloudPtr pc;
-        pc = open3d::geometry::PointCloud::CreateFromDepthImage(open3d_depth_legacy, camera_intrinsics, extrinsics, depth_scale);
+        pc = open3d::geometry::PointCloud::CreateFromDepthImage(open3d_depth_legacy, camera_intrinsics, extrinsics_eigen, depth_scale);
         pc->Transform(flip_mat);
 
         // Do prediction
@@ -152,12 +175,10 @@ int main(int argc, char* argv[])
             else
                 std::cout << "Inference failure" << std::endl;
         }
-        
-        //
 
         // Visualization
-        cv::imshow("color", image);
         cv::imshow("depth", depth);
+        cv::imshow("color", image);
 
         // If you want to create point cloud press 'p' or close the program with 'q' or 'esc'
         if (key == 'p')
@@ -177,4 +198,34 @@ int main(int argc, char* argv[])
     sensor.stopCapture();
   
     return 0;
+}
+
+
+void load_intrinsics(const std::string &filename, cv::Mat &intrinsics_out, cv::Mat &dist_coeff_out)
+{
+    cv::FileStorage fs("../config/calibration.yaml", cv::FileStorage::READ);
+    if (fs.isOpened())
+    {
+        fs["camera_matrix"] >> intrinsics_out;
+        fs["distortion_coefficients"] >> dist_coeff_out;
+        fs.release();
+    }
+    else
+    {
+        std::cout << "No intrinsics file found. Using default intrinsics or run the calibration script." << std::endl;
+    }
+}
+
+void load_extrinsics(const std::string &filename, cv::Mat &extrinsics)
+{
+    cv::FileStorage fs("../config/extrinsics.yaml", cv::FileStorage::READ);
+    if (fs.isOpened())
+    {
+        fs["extrinsics"] >> extrinsics;
+        fs.release();
+    }
+    else
+    {
+        std::cout << "No extrinsics file found. Using default extrinsics." << std::endl;
+    }
 }
